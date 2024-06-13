2f48f459b8aea714d0ae59d250dfca31a9f65c06
HaiyanKong <khy6@qq.com>
2024-06-13 17:07:32 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,23 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '27950e754aa74342b4090b83b1a3e561',
+  rootNotionPageId: '10a292e244d049bb8daea3ac9ad791a9?pvs=4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'rdfn penplotter studio',
-  domain: 'rdfn.ch',
-  author: 'Philipp Lehmann',
+  name: '',
+  domain: '',
+  author: '',
 
   // open graph metadata (optional)
-  description: 'A portfolio site for generative art',
+  description: '',
 
   // social usernames (optional)
-  // linkedin: 'ph.lehmann',
-  // instagram: 'ph.lehmann',
+  // twitter: 'transitive_bs',
+  // github: 'transitive-bullshit',
+  // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
@@ -30,7 +31,7 @@ export default siteConfig({
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: true,
+  isPreviewImageSupportEnabled: false,
 
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
@@ -41,24 +42,22 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/navi': '3b557d7ded3e4bfca78d78d1a6486781'},
+  // pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  // navigationStyle: 'default'
-  navigationStyle: 'custom',
-  navigationLinks: [
-    {
-      title: 'Photoplotter',
-      pageId: 'b870d9084d954356b00c04771ec03677'
-    }, 
-    {
-      title: 'About',
-      pageId: 'c0054a1d000446beb6baed4417066ed9'
-    }
-  ]
+  navigationStyle: 'default'
+  // navigationStyle: 'custom',
+  // navigationLinks: [
+  //   {
+  //     title: 'About',
+  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+  //   },
+  //   {
+  //     title: 'Contact',
+  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+  //   }
+  // ]
 })
